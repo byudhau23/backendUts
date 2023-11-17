c92d5ae335974a2603593aa327e1e952a6aca660
MSyaifulIslam-lab <syaifulislam4013@gmail.com>
2023-11-17 13:27:50 +0700

    uts1

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\News
+use App\Models\News;
 
 class NewsController extends Controller
 {
@@ -12,23 +12,42 @@ class NewsController extends Controller
      */
     public function index()
     {
-       
+		$news = News::all();
+
+		if (!empty($news)) {
+			$response = [
+				'message' => 'Menampilkan Semua Berita',
+				'data' => $news,
+			];
+			return response()->json($response, 200);
+		} else {
+			$response = [
+				'message' => 'Data tidak ada'
+			];
+			return response()->json($response, 404);
+		}
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        //
+        $input = $request->validate([
+                'title' => 'required',
+                'author' => 'requaired',
+                'description' => 'requaired',
+                'content' => 'requaired',
+                'url' => 'requaired',
+                'url_img' => 'requaired',
+                'published_at' => 'nullable',
+                'category' => 'requaired'
+        ]);
+        $news = News::create($input);
+
+		$response = [
+			'message' => 'Data Berita Berhasil Dibuat',
+			'data' => $news,
+		];
+
+		return response()->json($response, 201);
     }
 
     /**
@@ -36,15 +55,22 @@ class NewsController extends Controller
      */
     public function show(string $id)
     {
-        //
-    }
+        $news = News::find($id);
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
+		if ($news) {
+			$response = [
+				'message' => 'Get detail news',
+				'data' => $news
+			];
+	
+			return response()->json($response, 200);
+		} else {
+			$response = [
+				'message' => 'Data not found'
+			];
+			
+			return response()->json($response, 404);
+		}
     }
 
     /**
@@ -52,7 +78,22 @@ class NewsController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $news = News::find($id);
+
+		if ($news) {
+			$response = [
+				'message' => 'News is updated',
+				'data' => $news->update($request->all())
+			];
+	
+			return response()->json($response, 200);
+		} else {
+			$response = [
+				'message' => 'Data not found'
+			];
+
+			return response()->json($response, 404);
+		}
     }
 
     /**
@@ -60,6 +101,23 @@ class NewsController extends Controller
      */
     public function destroy(string $id)
     {
-        //
-    }
+        $news = News::find($id);
+
+		if ($news) {
+			$response = [
+				'message' => 'News is delete',
+				'data' => $news->delete()
+			];
+
+			return response()->json($response, 200); 
+		} else {
+			$response = [
+				'message' => 'Data not found'
+			];
+
+			return response()->json($response, 404);
+		}
+	}
+    
 }
+
